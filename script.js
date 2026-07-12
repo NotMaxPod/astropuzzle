1a376f084813d0f9b351322eb37592c258c95480
Kate <ekshubenok@gmail.com>
2026-07-12 12:40:34 +0200

    java height update

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ const viewMoreButtons = document.querySelectorAll(".view_more");
 viewMoreButtons.forEach(button => {
     button.addEventListener("click", () => {
         const card = button.closest(".session_card");
+        const dropdowndetails = card.querySelector(".details");
 
         card.classList.toggle("open");
 
         if (card.classList.contains("open")) {
+            dropdowndetails.style.maxHeight = dropdrowndetails.scrollHeight + "px";
             button.textContent = "Закрыть";
         } else {
+            dropdowndetails.style.maxHeight = "0px";
             button.textContent = "Подробнее";
         }
+    });
+});
+
+window.addEventListener("resize", () => {
+    const openCards = document.querySelectorAll(".session_card.open");
+    
+    openCards.forEach(card => {
+        const details = card.querySelector(".details");
+        dropdowndetails.style.maxHeight = dropdowndetails.scrollHeight + "px";
     });
 });
